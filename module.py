4ee8110a14374ed74dbabe9271200819cebda6b5
Niraj288 <32377509+Niraj288@users.noreply.github.com>
2017-10-11 11:02:20 -0500

    Update module.py

--- module.py
+++ module.py
@@ -1,11 +1,16 @@
 import os
 def search_deep(n_path,func,args=[]):
-	try:
-		for i in os.listdir(n_path):
-			if os.path.isdir(n_path+'/'+i):
-				search_deep(n_path+'/'+i,func,args=[])
-			else:
-				func(n_path+'/'+i)
-	except OSError:
-		func(n_path+'/'+i)
-		#raise Exception('Not a directory ')
+        try:
+                for i in os.listdir(n_path):
+                        if os.path.isdir(n_path+'/'+i):
+                                search_deep(n_path+'/'+i,func,args)
+                        else:
+                                for j in args:
+                                        if j==i[-len(j):]:
+                                                func(n_path+'/'+i)
+        except OSError:
+                for j in args:
+                        if j==i[-len(j):]:
+                                func(n_path+'/'+i)
+                
+                #raise Exception('Not a directory ')
