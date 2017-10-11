ba1abbf12121b4bc76e01259093d27ce9056ad78
Niraj288 <32377509+Niraj288@users.noreply.github.com>
2017-10-11 11:00:45 -0500

    Update check_cal.py

--- check_cal.py
+++ check_cal.py
@@ -2,29 +2,31 @@ import os
 import module
 
 def func(path):
-	if 'g16.out' not in path:
-		return
-	print path
-	ref,ref_f=0,1
-	file=open(path,'r')
-	file_=file.readlines()
-	file.close()
-	for line in file_:
-		if 'Frequencies' in line:
-			k=line.strip().split()
-			if float(k[3])<0:
-				ref_f=2
-				break
-			if float(k[2])<0:
-				ref_f=0
-				break
-	if ref_f==0:
-		print 'Imaginiary frequencies in '+path
-	elif ref_f==2:
-		print 'More or equal to 2 Imaginiary frequencies in '+path
-	if 'Normal termination of Gaussian' not in file_[-1]:
-		print 'Error termination in '+path
-	
+        if 'g16.out' not in path:
+                return
+        print path
+        ref,ref_f=0,1
+        file=open(path,'r')
+        file_=file.readlines()
+        file.close()
+        if 'Gaussian' not in file_[0]:
+                return
+        for line in file_:
+                if 'Frequencies' in line:
+                        k=line.strip().split()
+                        if float(k[3])<0:
+                                ref_f=2
+                                break
+                        if float(k[2])<0:
+                                ref_f=0
+                                break
+        if ref_f==0:
+                print 'Imaginiary frequencies in '+path
+        elif ref_f==2:
+                print 'More or equal to 2 Imaginiary frequencies in '+path
+        if 'Normal termination of Gaussian' not in file_[-1]:
+                print 'Error termination in '+path
+
 
 path=raw_input("Enter path : ")
 module.search_deep(path,func,['g16.out','g09.out'])
