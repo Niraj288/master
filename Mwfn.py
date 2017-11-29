2f413f6ca16f3eb7a36a20c208b17d3ec0118c15
Niraj Verma <nirajv@login02.cm.cluster>
2017-11-29 11:58:26 -0600

    Mwfn added

---
+++ Mwfn.py
@@ -0,0 +1,25 @@
+import os
+import threading
+import multiprocessing
+
+def gaussian(path,string):
+	filename=path[:-4]
+	os.system('mkdir '+filename)
+	os.chdir(filename)
+	os.system("mv ../"+path+' ./')
+	os.system("printf '"+string+"' | Multiwfn "+path)
+processes=[]
+for i in os.listdir('.'):
+	if i[-4:]=='.wfx':
+		p=multiprocessing.Process(target=gaussian, args=(i,'5\n1\n3\n2\n'))
+                processes.append(p)
+		q=multiprocessing.Process(target=gaussian, args=(i,'5\n12\n1\n2\n'))
+                processes.append(q)
+
+for p in processes:
+	p.start()
+
+
+print 'All calculation are done!!!'
+
+
